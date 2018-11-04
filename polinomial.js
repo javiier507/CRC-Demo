a64f81acc0bc6ac603d17b33dce8b1438c9d916b
javiier507 <javiier507@hotmail.com>
2018-11-04 12:24:20 -0500

    init project

---
+++ polinomial.js
@@ -0,0 +1,81 @@
+function convertirPolinomioEnBinario(formula)
+{
+	console.log(`\nFORMULA = ${formula}`);
+
+	let listaVariables = formula.split('+');
+
+	console.log(`\nVARIABLES = ${listaVariables}`);
+
+	let listaExponentes = listaVariables.map((item) => {
+
+		let variableItem = item.split('^');
+
+		console.log(`\nVARIABLES ITEM = ${variableItem}`);
+
+		let exponente = -1;
+		let flagNumber = false;
+
+		if(variableItem.length === 1) {
+			
+			flagNumber = isNaN(variableItem[0]); //	FALSE = NUMBER
+
+			if(flagNumber == true) {
+				exponente = 1;
+			}
+			else {
+				exponente = 0;
+			}
+
+		}
+		else if(variableItem.length === 2) {
+
+			flagNumber = isNaN(variableItem[1]); //	FALSE = NUMBER
+
+			if(!flagNumber) {
+				exponente = variableItem[1];
+				exponente = parseInt(exponente);
+			}
+
+		}
+
+		console.log(`EXPONENTE ITEM = ${exponente}`);
+
+		return exponente;
+	});
+
+	console.log(`\nEXPONENTES = ${listaExponentes}`);
+
+	let maximo = Math.max(...listaExponentes);
+
+	console.log(`\nMAX = ${maximo}`);
+
+	console.log('\nTRANSFORMACION A BINARIO');
+
+	if(maximo >= 0 && maximo <= 32) {
+
+		let resultado = [];
+		let binary;
+
+		for(let number = maximo; number >= 0; number--) {
+			
+			if(listaExponentes.includes(number)){
+				binary = 1;	
+			}
+			else {
+				binary = 0;
+			}
+
+			console.log(`ELEMENTO = ${number} ; ${binary}`);
+
+			resultado.push(binary);
+
+		}
+
+		console.log(`\nRESULTADO = ${formula} = ${resultado}`);
+	}
+}
+
+let formula = 'X^3+2';
+
+convertirPolinomioEnBinario(formula);
+
---
+++ webscrap.js
@@ -0,0 +1,25 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+    const browser = await puppeteer.launch({
+        headless: false,
+        slowMo: 300,
+        devtools: false
+    });
+    const page = await browser.newPage();
+    await page.goto('http://ascii2hex.com');
+
+    
+
+    const entrada = await page.$eval('#converter_text', (el) => el.value = 'HI');
+    console.log(`ENTRADA = ${entrada}`);
+
+    await page.click('input[name="convert_from_text"]');
+
+    const resultado = await page.$eval('#converter_binary', (el) => el.value);
+    console.log(`RESULTADO = ${resultado}`);
+
+    await page.waitForNavigation();
+
+    await browser.close();
+})();
